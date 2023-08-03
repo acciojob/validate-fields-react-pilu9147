5765e300dc20377faf13a2f8615356bb91c8ccdc
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-08-03 09:52:54 +0000

    vvvvvvvv

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,61 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      setErrorMessage("Please fill in all the fields.");
+    } else {
+      // Handle login logic here (e.g., make an API call to verify credentials)
+      // For this example, let's just display a success message
+      setErrorMessage("Login successful!");
+    }
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <div id="errorMessage">{errorMessage}</div>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default LoginForm;
