d1ef05b463dca02dcd31db92048c208a6441a0d0
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-08-03 09:59:05 +0000

    fffffffffffff

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ const LoginForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!formData.username || !formData.password) {
-      setErrorMessage("Please fill in all the fields.");
+      setErrorMessage("Both username and password are required");
     } else {
       // Handle login logic here (e.g., make an API call to verify credentials)
       // For this example, let's just display a success message
